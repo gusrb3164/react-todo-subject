cb1e9e02404bc8e5e410bd78cf120a36549dd830
gusrb3164 <gusrb3164@gmail.com>
2022-01-09 22:59:30 +0900

    feat: add main page

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+/**
+ * todo interface
+ * {
+ *   id: string
+ *   text: string
+ *   completed: boolean
+ *   createdAt: string
+ * }
+ */
+
+function App() {
+  return (
+    <main className="App">
+      <h1>Todo Project</h1>
+    </main>
+  );
+}
+
+export default App;
